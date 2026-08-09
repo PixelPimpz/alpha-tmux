4662aa5fba67031bccd5a588b51c8dd737b58ae3
Grady Broyles <slobeck@gmail.com>
2026-08-08 20:51:59 -0700

    Routine push: Sat Aug  8 08:51:59 PM PDT 2026

--- scripts/alpha-tmux.sh
+++ scripts/alpha-tmux.sh
@@ -3,7 +3,8 @@
 PLUGIN_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE="$PLUGIN_ROOT/img/tmux_fp-outline.png"
 TAGLINE="$( printf "Welcome to %s, %s! | %s" "$(tmux -V)" "$USER" "$(date "+%A, %b %d %Y")" )"
-
+#
+# ----- Helper functions
 center() {
   local row roww termw pad
   row="$1"
@@ -13,6 +14,15 @@ center() {
   printf "%*s%s\n" "$pad" "" "$row"
 }
 #
+is_session() {
+  local bool
+    if tmux ls >/dev/null 2>&1; then 
+      bool="true"
+    else bool="false"
+  fi
+  echo "$bool"
+}
+# ----- Generator functions 
 banner_gen() {
   while read -r line; do
     center "$line"
@@ -22,17 +32,15 @@ banner_gen() {
   done <<< "$TAGLINE"
 }
 #
-menu_gen() {
-  echo "teh menu, lolduh"
-}
-#
-#recents() {}
-#
-footer_gen() {
-  echo "I hope this satisfies the foot-people"
+menu_mockup() {
+  while read -r line; do
+    center "$line"
+  done <<< "$(echo -e "[x] X button1\t[x] X button2\t[x] X button3\t[x] X button4\t[x] X button5\t" | boxes -d ansi)"
 }
 #
 main() {
   banner_gen
+  center " "
+  menu_mockup
 }
 main
