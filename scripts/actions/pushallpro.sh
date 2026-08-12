9dcd251ac599b99b761aa7a573258b449b907603
Grady Broyles <slobeck@gmail.com>
2026-08-12 12:40:53 -0700

    Routine push: Wed Aug 12 12:40:53 PM PDT 2026

--- scripts/actions/pushallpro.sh
+++ scripts/actions/pushallpro.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 ls -1 "$TMUX_PLUGIN_MANAGER_PATH/"
-read -rp "press any key to return to Alpha" -n 1 key 
+ITEMC="$(find -d "$TMUX_PLUGIN_MANAGER_PATH/" | wc -l)"
+REPO="$( git info | grep -e "(push)" | awk '{print $2}' )"
+read -rp "Push $ITEMC projects to $REPO? [Y|n]" -n 1 key 
+case "$key" in
+  [nN])
+    exit ;;
+  [yY])
+    while read -r line; do
+      echo "$line"
+    done
+    ;;
+  *)
+    echo "$key no recognized" 
+    exit ;;
+esac
