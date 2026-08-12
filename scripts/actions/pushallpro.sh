f7590c497096db1e4a49777157f0c44100640a92
Grady Broyles <slobeck@gmail.com>
2026-08-12 12:51:33 -0700

    Routine push: Wed Aug 12 12:51:33 PM PDT 2026

--- scripts/actions/pushallpro.sh
+++ scripts/actions/pushallpro.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ls -1 "$TMUX_PLUGIN_MANAGER_PATH/"
-ITEMC="$(find -type d "$TMUX_PLUGIN_MANAGER_PATH/" | wc -l)"
+ITEMC="$(find "$TMUX_PLUGIN_MANAGER_PATH/" -type d | wc -l)"
 REPO="$( git info | grep -e "(push)" | awk '{print $2}' )"
 read -rp "Push $ITEMC projects to $REPO? [Y|n]" -n 1 key 
 case "$key" in
