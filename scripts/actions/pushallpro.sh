e8c9d344c0d0b47a1cc0288f49e9ffa8e701d5a8
Grady Broyles <slobeck@gmail.com>
2026-08-12 13:07:04 -0700

    Routine push: Wed Aug 12 01:07:04 PM PDT 2026

--- scripts/actions/pushallpro.sh
+++ scripts/actions/pushallpro.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
-ls -1 "$TMUX_PLUGIN_MANAGER_PATH/"
-ITEMC="$(find "$TMUX_PLUGIN_MANAGER_PATH/" -type d | wc -l)"
-REPO="$( git info | grep -e "(push)" | awk '{print $2}' )"
-read -rp "Push $ITEMC projects to $REPO? [Y|n]" -n 1 key 
-case "$key" in
-  [nN])
-    exit ;;
-  [yY])
-    while read -r line; do
-      echo "$line"
-    done
-    ;;
-  *)
-    echo "$key no recognized" 
-    exit ;;
-esac
+fatal() {
+  local E
+  E="${1:-Unknown Error}"
+  echo "Error: $E."
+  read -r -p "Press any key to return to Alpha-Tmux." -n 1 key
+  exit
+}
+
+if [[ ! -d "$PROJECTS" ]]; then
+  fatal "No \"Projects\" directory specified."
+else
+  ls -1 "$PROJECTS/"
+  ITEMC="$(find "$PROJECTS/" -type d | wc -l)"
+  REPO="$( git info | grep -e "(push)" | awk '{print $2}' )"
+  read -rp "Push $ITEMC projects to $REPO? [Y|n]" -n 1 key 
+  case "$key" in
+    [nN])
+      exit ;;
+    [yY])
+      while read -r line; do
+        echo "LINE: $line"
+      done < <(find "$PROJECTS/" -type d)
+      ;;
+    *)
+      echo "$key no recognized" 
+      exit ;;
+  esac
+fi
