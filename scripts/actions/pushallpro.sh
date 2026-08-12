833233fe6ebe0bbb51828460194cb47d76678758
Grady Broyles <slobeck@gmail.com>
2026-08-12 13:34:24 -0700

    Routine push: Wed Aug 12 01:34:24 PM PDT 2026

--- scripts/actions/pushallpro.sh
+++ scripts/actions/pushallpro.sh
@@ -7,24 +7,28 @@ fatal() {
   exit
 }
 
-if [[ ! -d "$PROJECTS" ]]; then
-  fatal "No \"Projects\" directory specified."
-else
-  #ls -1 "$PROJECTS/"
-  find "$PROJECTS" -maxdepth 1 -type d 
-  ITEMC="$(find "$PROJECTS/" -maxdepth 1 -type d | wc -l)"
-  REPO="$( git info | grep -e "(push)" | awk '{print $2}' )"
-  read -rp "Push $ITEMC projects to $REPO? [Y|n]" -n 1 key 
-  case "$key" in
-    n|N)
-      exit ;;
-    y|Y)
-      while read -r line; do
-        echo "LINE: $line"
-      done < <(find "$PROJECTS/" -maxdepth 1 -type d)
-      ;;
-    *)
-      echo "$key no recognized" 
-      exit ;;
-  esac
-fi
+main() {
+  local key
+  if [[ ! -d "$PROJECTS" ]]; then
+    fatal "No \"Projects\" directory specified."
+  else
+    find "$PROJECTS" -mindepth 1 -maxdepth 1 -type d 
+    ITEMC="$(find "$PROJECTS/" -mindepth -maxdepth 1 -type d | wc -l)"
+    REPO="$( git info | grep -e "(push)" | awk '{print $2}' )"
+    read -rp "Push $ITEMC projects to $REPO? [Y|n]" -n 1 key 
+    key="${key:-Y}"
+    case "$key" in
+      n|N)
+        exit ;;
+      y|Y)
+        while read -r line; do
+          echo "LINE: $line"
+        done < <(find "$PROJECTS/" -mindepth -maxdepth 1 -type d)
+        ;;
+      *)
+        echo "$key no recognized" 
+        exit ;;
+    esac
+  fi
+}
+main
