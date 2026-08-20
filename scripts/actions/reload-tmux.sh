1222b9a6c26eb4c0d86372b82aef79c4be11fcd5
Grady Broyles <slobeck@gmail.com>
2026-08-20 03:09:09 -0700

    Routine push: Thu Aug 20 03:09:09 AM PDT 2026

--- scripts/actions/reload-tmux.sh
+++ scripts/actions/reload-tmux.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-tmux display-popup -E | source $TMUX_CONFIG; echo 'tmux.conf reloaded...'
-sleep 5
+SCRIPT_PATH="$( readlink -f "${BASH_SOURCE[0]}" )"
+PLUGIN_ROOT="$( cd "$( dirname "$SCRIPT_PATH" )/../.." && pwd )"
+source "$PLUGIN_ROOT/scripts/utils/formats.sh"
+ca
+tmux display-popup -E | tmux source-file "$HOME/.config/tmux/tmux.conf"; echo 'tmux.conf reloaded...'
+sleep 3
