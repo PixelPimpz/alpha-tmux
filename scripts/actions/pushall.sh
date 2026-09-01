b81db04e53dae23d2fa3c438c90d4fba6bd6bfcb
Grady Broyles <slobeck@gmail.com>
2026-09-01 09:56:10 -0700

    Routine commit:  01-09-2026 09:56:10

--- scripts/actions/pushall.sh
+++ scripts/actions/pushall.sh
@@ -34,7 +34,8 @@ main() {
     else
       icon="$skip"
     fi
-    printf "\t%s%-3s%s %s%s\n" "${MENUKEYC}" "$icon" "${TEXTC}" "${f/"$HOME"/~}" "${RESET}"
+    f="${f/$HOME/~}"
+    printf "\t%s%-3s%s %s%s\n" "${MENUKEYC}" "$icon" "${TEXTC}" "${f}" "${RESET}"
   done
 
   # The doctor wants you to PUUUUUUSH!
